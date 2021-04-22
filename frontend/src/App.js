e06f8721a3135d1b6a40e81118c86cb28b37affd
Pete Low <pedritolow@gmail.com>
2021-04-21 17:18:42 -0700

    added HomeScreen and Product

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,19 @@
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
 
 const App = () => {
   return (
     <>
     <Header />
     <main className='py-3'>
-    <Container><h1>Welcome to Proshop</h1></Container>
+    <Container>
+      <HomeScreen />
+    </Container>
     </main>
     <Footer />
+
     </>
   );
 }
