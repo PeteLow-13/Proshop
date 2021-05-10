e886b7cf52629a2201daf7c17d081a71c295050d
Pete Low <pedritolow@gmail.com>
2021-05-10 15:17:28 -0700

    added auth middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,6 +22,23 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(401);
     throw new Error('invalid Email or Password');
   }
+}); // @desc   Get user profile
+// @route  POST /api/users/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
-export { authUser };
+export { authUser, getUserProfile };
