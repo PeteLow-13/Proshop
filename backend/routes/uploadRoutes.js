08061abd8e4b60d441cb7a6fe7078135890369e4
Pete Low <pedritolow@gmail.com>
2021-07-03 12:52:16 -0700

    finished uploadRoutes

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -10,13 +10,32 @@ const storage = multer.diskStorage({
   filename(req, file, cb) {
     cb(
       null,
-      `${file.fieldname}-${Date.now()}${path.ext.name(file.originalname)}`
+      `${file.fieldname}-${Date.now()}${path.extname(file.originalname)}`
     );
   },
 });
 
+function checkFileType(file, cb) {
+  const filetypes = /jpg|jpeg|png/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  const mimetype = filetypes.test(file.mimetype);
+
+  if (extname && mimetype) {
+    return cb(null, true);
+  } else {
+    cb('Images only!');
+  }
+}
+
 const upload = multer({
   storage,
+  fileFilter: function (req, file, cb) {
+    checkFileType(file, cb);
+  },
+});
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.send(`/${req.file.path}`);
 });
 
 export default router;
