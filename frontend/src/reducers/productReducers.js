0c009748848721bf750894ac26e87af65423cb86
Pete Low <pedritolow@gmail.com>
2021-07-16 14:18:36 -0700

    productUpdateReducer

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -16,6 +16,10 @@ import {
   PRODUCT_UPDATE_REQUEST,
   PRODUCT_UPDATE_FAIL,
   PRODUCT_UPDATE_RESET,
+  PRODUCT_CREATE_REVIEW_RESET,
+  PRODUCT_CREATE_REVIEW_FAIL,
+  PRODUCT_CREATE_REVIEW_SUCCESS,
+  PRODUCT_CREATE_REVIEW_REQUEST,
 } from '../constants/productConstants';
 
 export const productListReducer = (state = { products: [] }, action) => {
@@ -75,21 +79,6 @@ export const productCreateReducer = (state = {}, action) => {
   }
 };
 
-export const productReviewCreateReducer = (state = {}, action) => {
-  switch (action.type) {
-    case PRODUCT_CREATE_REQUEST:
-      return { loading: true };
-    case PRODUCT_CREATE_SUCCESS:
-      return { loading: false, success: true, product: action.payload };
-    case PRODUCT_CREATE_FAIL:
-      return { loading: false, error: action.payload };
-    case PRODUCT_CREATE_RESET:
-      return {};
-    default:
-      return state;
-  }
-};
-
 export const productUpdateReducer = (state = { product: {} }, action) => {
   switch (action.type) {
     case PRODUCT_UPDATE_REQUEST:
@@ -100,6 +89,21 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
       return { loading: false, error: action.payload };
     case PRODUCT_UPDATE_RESET:
       return { product: {} };
+    default:
+      return state;
+  }
+};
+
+export const productReviewCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_CREATE_REVIEW_REQUEST:
+      return { loading: true };
+    case PRODUCT_CREATE_REVIEW_SUCCESS:
+      return { loading: false, success: true };
+    case PRODUCT_CREATE_REVIEW_FAIL:
+      return { loading: false, error: action.payload };
+    case PRODUCT_CREATE_REVIEW_RESET:
+      return {};
     default:
       return state;
   }
